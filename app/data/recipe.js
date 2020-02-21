3013fe5f68721728557909ae9f9a746668f72885
abassade <abs4real16.ma@gmail.com>
2020-02-21 01:38:11 +0100

    first commit

---
+++ app/data/recipe.js
@@ -0,0 +1,8 @@
+export default [
+    {   
+        id: 1,
+        name: "gbegiri",
+        description: "It is a yoruba local soup that is suitable on amala",
+        addedAt: "2020-02-21T00:32:15.254Z"
+    }
+];
---
+++ app/routes/recipe.js
@@ -0,0 +1,9 @@
+import express from "express";
+import recipeController from "../controllers/recipe";
+
+export default (app)=>{
+    // recipe endpoints
+    app.get("/api/v1/", (req, res)=> res.status(200).json({"Recipe API": "Healthy"}))
+    app.get("/api/v1/recipe", recipeController.getRecipes);
+    app.post("/api/v1/recipe", recipeController.addRecipe);   
+}
